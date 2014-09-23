8cec46dc3c6b6ab933bc4504afaea23e65c9a234
Richard Harrah <topplethenunnery@gmail.com>
2014-09-23 00:01:54 -0400

    adding shapeless recipe support

--- src/main/java/info/faceland/bellows/BellowsPlugin.java
+++ src/main/java/info/faceland/bellows/BellowsPlugin.java
@@ -17,6 +17,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -54,26 +55,42 @@ public class BellowsPlugin extends FacePlugin {
             }
             String name = TextUtils.color(recipes.getString(key + ".name", ""));
             List<String> lore = TextUtils.color(recipes.getStringList(key + ".lore"));
-            List<String> recipeList = recipes.getStringList(key + ".recipe");
-            String[] recipeArray = recipeList.toArray(new String[recipeList.size()]);
-            Map<String, Material> materialMap = new HashMap<>();
-            ConfigurationSection cs = recipes.getConfigurationSection(key + ".ingredients");
-            for (String mKey : cs.getKeys(false)) {
-                Material m = Material.getMaterial(cs.getString(mKey));
-                if (m == null || m == Material.AIR) {
-                    continue;
-                }
-                materialMap.put(mKey, m);
-            }
             HiltItemStack hiltItemStack = new HiltItemStack(material);
             hiltItemStack.setName(name);
             hiltItemStack.setLore(lore);
-            ShapedRecipe recipe = new ShapedRecipe(hiltItemStack);
-            recipe.shape(recipeArray);
-            for (Map.Entry<String, Material> e : materialMap.entrySet()) {
-                recipe.setIngredient(e.getKey().charAt(0), e.getValue());
+            if (recipes.isSet(key + ".shaped-recipe")) {
+                List<String> recipeList = recipes.getStringList(key + ".shaped-recipe");
+                String[] recipeArray = recipeList.toArray(new String[recipeList.size()]);
+                Map<String, Material> materialMap = new HashMap<>();
+                ConfigurationSection cs = recipes.getConfigurationSection(key + ".ingredients");
+                for (String mKey : cs.getKeys(false)) {
+                    Material m = Material.getMaterial(cs.getString(mKey));
+                    if (m == null || m == Material.AIR) {
+                        continue;
+                    }
+                    materialMap.put(mKey, m);
+                }
+                ShapedRecipe recipe = new ShapedRecipe(hiltItemStack);
+                recipe.shape(recipeArray);
+                for (Map.Entry<String, Material> e : materialMap.entrySet()) {
+                    recipe.setIngredient(e.getKey().charAt(0), e.getValue());
+                }
+                Bukkit.addRecipe(recipe);
+            } else {
+                ConfigurationSection shapeless = recipes.getConfigurationSection(key + ".shapeless-recipe");
+                if (shapeless == null) {
+                    continue;
+                }
+                ShapelessRecipe recipe = new ShapelessRecipe(hiltItemStack);
+                for (String ing : shapeless.getKeys(false)) {
+                    Material m = Material.getMaterial(ing);
+                    if (m == null || m == Material.AIR) {
+                        continue;
+                    }
+                    recipe.addIngredient(shapeless.getInt(ing), m);
+                }
+                Bukkit.addRecipe(recipe);
             }
-            Bukkit.addRecipe(recipe);
         }
     }
 
