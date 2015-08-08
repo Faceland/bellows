d0adfc439e65e531a4e46ce52dc8c300ac6d67d4
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2015-08-08 00:21:08 -0400

    Fixing unstackable craftables

--- src/main/java/info/faceland/bellows/BellowsPlugin.java
+++ src/main/java/info/faceland/bellows/BellowsPlugin.java
@@ -144,7 +144,13 @@ public class BellowsPlugin extends FacePlugin {
                     Joiner.on(" ").skipNulls().join(is.getType().name().split("_"))))) {
                 hiltItemStack.setName(TextUtils.color(name));
                 hiltItemStack.setLore(color(lore));
-                hiltItemStack.setItemFlags(Sets.newHashSet(ItemFlag.HIDE_ATTRIBUTES));
+                if (is.getType() == Material.WOOD_AXE || is.getType() == Material.WOOD_SWORD ||
+                    is.getType() == Material.STONE_AXE || is.getType() == Material.STONE_SWORD ||
+                    is.getType() == Material.IRON_AXE || is.getType() == Material.IRON_SWORD ||
+                    is.getType() == Material.GOLD_AXE || is.getType() == Material.GOLD_SWORD ||
+                    is.getType() == Material.DIAMOND_AXE || is.getType() == Material.DIAMOND_SWORD) {
+                    hiltItemStack.setItemFlags(Sets.newHashSet(ItemFlag.HIDE_ATTRIBUTES));
+                }
                 event.getInventory().setResult(hiltItemStack);
             }
         }
